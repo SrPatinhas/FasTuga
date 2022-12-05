ca125c0b91be712e7741f1c5bf43c92befe016ea
Miguel Cerejo <migas_cerejo-1993@hotmail.com>
2022-12-05 22:22:13 +0000

    work on node server

--- websocket_server/index.js
+++ websocket_server/index.js
@@ -1,4 +1,4 @@
-const httpServer = require('http').createServer()
+const httpServer = require('http').createServer();
 const io = require("socket.io")(httpServer, {
     cors: {
         // The origin is the same as the Vue app domain:
@@ -10,30 +10,108 @@ const io = require("socket.io")(httpServer, {
 })
 httpServer.listen(8080, () =>{
     console.log('listening on *:8080')
-})
+});
+
+let SessionHandler = require("./SessionHandler.js");
+let sessions = new SessionHandler();
+
 io.on('connection', (socket) => {
-    console.log(`client ${socket.id} has connected`)
-    socket.on('newProject', (project) => {
-        socket.broadcast.emit('newProject', project)
-    })
-    socket.on('updateProject', (project) => {
-        socket.broadcast.emit('updateProject', project)
-    })
-    socket.on('deleteProject', (project) => {
-        socket.broadcast.emit('deleteProject', project)
-    })
-    socket.on('loggedIn', function (user) {
-        socket.join(user.id)
-        if (user.type == 'A') {
-            socket.join('administrator')
+    console.log(`client ${socket.id} has connected`);
+
+    /****************************************
+     *      Events to the "restaurant"      *
+     ***************************************/
+    /**
+     * Events to the "restaurant_kitchen"
+     **/
+    socket.on('orderNew', (order) => {
+        io.to("restaurant").emit('orderNew', order); // sends the new order
+    });
+    /**
+     * Events to the "restaurant_counter"
+     **/
+    socket.on('ticketNew', (ticket) => {
+        io.to("restaurant").emit('ticketNew', ticket)
+    });
+
+    /*
+     * Events to the "client"
+     */
+    socket.on('orderUpdateStatus', (idUser_Order, order) => {
+        const orderSession = sessions.getUserSession(idUser_Order);
+        if(orderSession){ // receives the id of the user to be notified
+            io.to(orderSession.socketID).emit('orderUpdateStatus', order); // sends the new order
         }
-    })
-    socket.on('loggedOut', function (user) {
-        socket.leave(user.id)
-        socket.leave('administrator')
-    })
-    socket.on('updateUser', function (user) {
-        socket.in('administrator').except(user.id).emit('updateUser', user)
-        socket.in(user.id).emit('updateUser', user)
-    })
+    });
+    socket.on('orderCanceled', (idUser_Order) =>{
+        const orderSession = sessions.getUserSession(idUser_Order);
+        if(orderSession){ // receives the id of the user to be notified
+            io.to(orderSession.socketID).emit('orderCanceled');
+        }
+    });
+    socket.on('refundOrder', (idUser_Order, order) => {
+        const orderSession = sessions.getUserSession(idUser_Order);
+        if(orderSession){ // receives the id of the user to be notified
+            io.to(orderSession.socketID).emit('refundOrder', order);
+        }
+    });
+
+
+    socket.on('ticketComplete', (idUser_Order, order) => {
+        const orderSession = sessions.getUserSession(idUser_Order);
+        if(orderSession){ // receives the id of the user to be notified
+            io.to(orderSession.socketID).emit('ticketComplete', order);
+        }
+    });
+
+    /*
+     * Login user
+     * Register Log
+     * Join user to the correct room
+     */
+    socket.on('loggedIn', (user) => {
+        if (user) {
+            sessions.addUserSession(user, socket.id);
+            socket.join(user.type);
+            //updated user availability
+            axios.put(url + '/api/updateLoggedAt', {"user_id": user.id, "logged": 1}).then(response =>{
+                if (user.type == 'A') {
+                    socket.join('administrator');
+                }
+            }).catch(error => {
+                console.log("Login - Error: " + error.data);
+            });
+        }
+    });
+
+    socket.on('loggedOut', (user) => {
+        if (user) {
+            socket.leave(user.type);
+            sessions.removeUserSession(user.id);
+        }
+    });
+
+    socket.on('disconnect', (reason) => {
+        let x = sessions.removeSocketIDSessionAndGetId(socket.id);
+        if(x == null){
+            return; // user fez logout antes de fechar a página.
+        }
+        axios.put(url + '/api/updateLoggedAt', {"user_id": x.id, "logged": 0}).then(response =>{
+            if(x.type !== "A"){
+                io.to("restaurant").emit('update_incoming');
+            }
+        }).catch(error => {
+            console.log("disconnect - Error: " + error.data);
+        });
+    });
+
+    socket.on('changeBlockedStatus', (user) => {
+        // verficar se user está logado
+        let session = sessions.getUserSession(user.id);
+        if (session && user.blocked == 1){
+            io.to(`${session.socketID}`).emit('blockStatusUpdate')
+        }
+        //if another manager is logged
+        io.to('EM').emit('updateUserList', user);
+    });
 })
