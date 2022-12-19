a83577ca9fa48c25ddf426d7a606d97db9e5c21e
JeGracio <58191429+JeGracio@users.noreply.github.com>
2022-12-19 17:04:14 +0000

    Settings product get

--- app_client/src/components/settings/Products.vue
+++ app_client/src/components/settings/Products.vue
@@ -1,17 +1,18 @@
 <script setup>
-	import SettingsBar from "@/components/navigation/SettingsBar.vue"
-	import SettingsMenu from "@/components/products/ProductSettingsItem.vue"
+import SettingsMenu from "@/components/products/ProductSettingsItem.vue"
+import Pagination from "../navigation/Pagination.vue"
 </script>
 
 <template>
 	<!-- Content-->
 	<section class="col-lg-8 pt-lg-4 pb-4 mb-3">
 		<div class="pt-2 px-4 ps-lg-0 pe-xl-5">
-			<h2 class="h3 py-2 text-center text-sm-start">Menus</h2>
+			<h2 class="h3 py-2 text-center text-sm-start">Products</h2>
 			<!-- Tabs-->
 			<div class="table-responsive fs-md mb-4">
 				<SettingsMenu></SettingsMenu>
 			</div>
 		</div>
 	</section>
+	<Pagination></Pagination>
 </template>
