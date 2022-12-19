30a13e8c11eaadee2c10b0f0eaaaed771606885e
JeGracio <58191429+JeGracio@users.noreply.github.com>
2022-12-19 17:32:26 +0000

    Image correction in products settings

--- app_client/src/components/products/ProductSettingsItem.vue
+++ app_client/src/components/products/ProductSettingsItem.vue
@@ -12,7 +12,9 @@
 
 <template>
 		<div class="d-block d-sm-flex align-items-center py-4 border-bottom" v-for="(product,index) of productStore.products.data" :key="index">
-			<a class="d-block mb-3 mb-sm-0 me-sm-4 ms-sm-0 mx-auto" href="marketplace-single.html" style="width: 12.5rem;"><img :src="photo_url" class="card-img-top" :alt="name"></a>                  
+			<div class="d-block mb-3 mb-sm-0 me-sm-4 ms-sm-0 mx-auto" style="width: 12.5rem;">
+				<img :src="product.photo_url" class="card-img-top" :alt="name">
+			</div>                  
 			<div class="text-center text-sm-start">
                     <h3 class="h6 product-title mb-2">{{ product.name }}</h3>
                     <div class="d-inline-block text-accent">{{ product.description }}</div>
